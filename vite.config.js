f1d70097aed61a80459a8aa3de7af9bd9f5d9db7
Rafael Carro <rafaelcgaudim@yahoo.com.br>
2025-09-02 22:15:50 -0300

    Added github pages script

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,10 @@
-import { defineConfig } from 'vite'
+import { defineConfig } from "vite"
 
 export default defineConfig({
-  base: '/ThreeSolarSystemModel/', // Replace with your actual repository name
+  base: "/ThreeSolarSystemModel/", // Replace with your actual repository name
   build: {
-    outDir: 'dist',
-    assetsDir: 'assets',
-    rollupOptions: {
-      input: {
-        main: './index.html'
-      }
-    }
+    outDir: "dist",
+    assetsDir: "assets"
   },
   server: {
     port: 3000,
